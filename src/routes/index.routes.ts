e05ec4c0adc244edb822febf0196e8bbb03c0bd5
Sr1515 <joao151576@gmail.com>
2024-04-23 15:10:45 -0300

    Bug fix

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -4,6 +4,7 @@ import { createProductController } from "../useCases/CreateProduct"
 import { createPersonController } from "../useCases/CreatePerson"
 import { createSupplyController } from "../useCases/CreateSupply"
 import { findAllPersonController } from "../useCases/FindAllPerson"
+import { findAllSuppliersController } from "../useCases/FindAllSupplier"
 
 const router: Router = Router()
 
@@ -12,7 +13,7 @@ router.post("/product", (request, response) => createProductController.handle(re
 router.post("/person", (request, response) => createPersonController.handle(request, response))
 router.post("/supply", (request, response) => createSupplyController.handle(request, response))
 
-router.get("/supplier", (request, response) => findAllPersonController.handle(request, response)) 
+router.get("/supplier", (request, response) => findAllSuppliersController.handle(request, response)) 
 router.get("/person", (request, response) => findAllPersonController.handle(request, response))
 
 
