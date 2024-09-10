f72bbfc9c3f53cac61f0d1cad5d5b2327945a1c2
Sr1515 <joao151576@gmail.com>
2024-09-10 09:01:37 -0300

    some product test implemented

--- src/__test__/container.test.ts
+++ src/__test__/container.test.ts
@@ -1,43 +1,44 @@
 import {
     TestContainer,
     StartedTestContainer,
-    StoppedTestContainer,
     GenericContainer
-} from "testcontainers"
-import { SupplierRepository } from "../repositories/implementations/SupplierRepository"
-import { CreateSupplierUseCase } from "../useCases/CreateSupplier/createSupplierUseCase"
-import sequelizeInit from "../database/connection"
+} from "testcontainers";
+import { SupplierRepository } from "../repositories/implementations/SupplierRepository";
+import { CreateSupplierUseCase } from "../useCases/CreateSupplier/createSupplierUseCase";
+import sequelizeInit from "../database/connection";
 
 describe("Testes com tabela Person sem autentificação", () => {
-    jest.setTimeout(15000)
-    let sequelize: any = null
-    let startedContainer: StartedTestContainer
+    jest.setTimeout(150000);
+    let sequelize: any = null;
+    let startedContainer: StartedTestContainer;
 
     beforeAll(async () => {
-        const container: TestContainer = new GenericContainer("postgis/postgis:latest")
+        const container: TestContainer = new GenericContainer("postgis/postgis:12-3.0");
         startedContainer = await container
-        .withEnvironment({"POSTGRES_USER": "admin"})
-        .withEnvironment({"POSTGRES_DB": "main"})
-        .withEnvironment({"POSTGRES_PASSWORD": "123"})
-        .start()
-        sequelize = sequelizeInit("main", "123","admin", startedContainer.getHost())
-        await sequelize.sync()
-    })
+            .withEnvironment({"POSTGRES_USER": "admin"})
+            .withEnvironment({"POSTGRES_DB": "main"})
+            .withEnvironment({"POSTGRES_PASSWORD": "123"})
+            .start();
+
+        sequelize = sequelizeInit("main", "123", "admin", startedContainer.getHost());
+        await sequelize.authenticate(); 
+        await sequelize.sync();
+        console.log("Conexão com o banco de dados estabelecida e sincronizada.");
+    });
 
     afterAll(async () => {
-            await startedContainer.stop()
-            await sequelize.close()
-        }
-    )
+        await sequelize.close();
+        await startedContainer.stop();
+    });
 
     test("Deve adicionar e selecionar uma pessoa", async () => {
-        const supplierRepository = new SupplierRepository(sequelize)
-        const createSupplierUseCase = new CreateSupplierUseCase(supplierRepository)
-        let data = {
+        const supplierRepository = new SupplierRepository(sequelize);
+        const createSupplierUseCase = new CreateSupplierUseCase(supplierRepository);
+        const data = {
             name: 'JV',
             password: 'bandido'
-        }
-        
-        expect(async () => {return await createSupplierUseCase.execute(data)}).not.toThrow()
-    })
-})
+        };
+
+        await expect(createSupplierUseCase.execute(data)).resolves.not.toThrow();
+    });
+});
